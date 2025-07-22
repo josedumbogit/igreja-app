a291848b9f13d9c4cdea48b933c9f691122a754c
josedumbogit <bernardoxyloto@gmail.com>
2025-07-22 16:05:09 +0100

    Novo Design2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="d-flex justify-content-center align-items-center" style={{ height: '80vh' }}>
       <Card className="text-center p-5 shadow-lg">
-        <h1 className="mb-4">Bem-vindo ao Sistema de Gestão de Informações da Igreja</h1>
+        <h1 className="mb-4">Bem-vindo ao Sistema de Gestão de Informações de Igrejas</h1>
         <p>Utilize o menu lateral para navegar pelas funcionalidades do sistema.</p>
       </Card>
     </div>
