a68f283fafcd387e3ae442f341859999badac55d
josedumbogit <bernardoxyloto@gmail.com>
2025-07-23 11:55:02 +0100

    Novo Estilo Bootstrap2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="text-center">
-      <h1 className="mb-4">Bem-vindo ao Sistema de Gestão de Informação de Igrejas</h1>
+      <h1 className="mb-4">Bem-vindo ao Sistema de Gestão de Informação da ADP</h1>
       <p className="lead">Utilize o menu lateral para navegar pelas funcionalidades do sistema.</p>
       <img
         src="https://source.unsplash.com/600x300/?church"
